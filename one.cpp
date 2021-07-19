7a6717413ff55f83bb36fc95bdaca2c9698bb9df
Tushar Saxena <stushar345@gmail.com>
2021-07-19 15:11:44 +0530

    Update one.cpp

--- one.cpp
+++ one.cpp
@@ -3,64 +3,78 @@ using namespace std;
 # define N 1000000
 vector<long long int> gr[N];
 // Function to add edge
-void Add_edge(long long int x, long long int y)
+void add_edge(long long int x, long long int y)
 {
-	gr[x].push_back(y);
+    gr[x].push_back(y);
     gr[y].push_back(x);
 }
 
 
-long long int DFS(long long int source,long long int visited[],long long int connected)
-{
-visited[source]=1;
-for(auto x: gr[source])
-{
-    if(visited[x]==0)
+int bfs(long long int source,long long int visited[])
+{    
+   long long int distance=1;
+    queue<int> q;
+    q.push(source);
+    visited[source]=1;
+
+    while(!q.empty())
     {
-        connected++;
-        connected=DFS(x,visited,connected);
+        long long int node=q.front();
+        q.pop();
+        for(long long int x: gr[node])
+        {
+            if(visited[x]==-1)
+            {
+                distance++;
+                q.push(x);
+                visited[x]=1;
+            }
+
+        }
     }
-}
-return connected;
+return distance;
 }
 
 
 int main()
 {
-long long int n;
-cout<<"\nEnter the number of nodes: ";
-cin>>n;
-long long int pairs;
-cout<<"\nEnter the number of pairs: ";
-cin>>pairs;
-cout<<"\nEnter the pairs:\n";
-for(long long int i=0;i<pairs;i++)
+long long int vertices,edges;
+//cout<<"Enter the number of vertices and edges: "<<endl;
+cin>>vertices>>edges;
+
+for(int i=0;i<edges;i++)
 {
-    long long int x,y;
+    int x,y;
+    //cout<<"Enter the graph: "<<endl;
     cin>>x>>y;
-    Add_edge(x,y);
+    add_edge(x,y);
 }
 
-long long int visited[n]={0};
-long long int connected_components=1;
-long long int connected[n]={0};
-long long int count=0;
-for(long long int i=0;i<n;i++)
+long long int visited[vertices];
+
+for(int i=0;i<vertices;i++)
 {
-    if(visited[i]==0)
+    visited[i]=-1;
+}
+
+vector<long long int>count;
+
+for(long long int i=0;i<vertices;i++)
+{
+    if(visited[i]==-1)
     {
-        long long int x=DFS(i,visited,connected_components);
-        connected[count]=x;
-        count++;
+        long long int dist=bfs(i,visited);
+        count.push_back(dist);
     }
 }
 
-long long int total=n*(n-1)/2;
 
-for(long long int i=0;i<count;i++)
+long long int total=0;
+total=vertices*(vertices-1)/2;
+
+for(long long int i=0;i<count.size();i++)
 {
-	total=total-(connected[i]*(connected[i]-1)/2);
+    total=total-(count[i]*(count[i]-1)/2);
 }
-
 cout<<total;
 }
